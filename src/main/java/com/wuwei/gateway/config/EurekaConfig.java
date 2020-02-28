e3c4878ac1970f5355f9b3274f4c038269ed8d7c
吴威 <664507439@qq.com>
2020-02-28 16:23:53 +0800

    eureka依赖误删除

--- src/main/java/com/wuwei/gateway/config/EurekaConfig.java
+++ src/main/java/com/wuwei/gateway/config/EurekaConfig.java
@@ -3,7 +3,7 @@ package com.wuwei.config.config;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * @Author: yexiaodong
+ * @Author: wuwei
  * @Date:2018/9/8 11:02
  */
 @EnableEurekaClient
