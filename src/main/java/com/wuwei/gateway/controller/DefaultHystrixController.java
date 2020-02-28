4ebe805fd1f78a6a9e778b7dc0049b16653b051b
吴威 <664507439@qq.com>
2020-02-29 00:33:26 +0800

    自定义熔断

---
+++ src/main/java/com/wuwei/gateway/controller/DefaultHystrixController.java
@@ -0,0 +1,26 @@
+package com.wuwei.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: wuwei
+ * @Date:2020-02-28 22:53
+ * 默认降级处理
+ */
+@RestController
+public class DefaultHystrixController {
+
+    @RequestMapping("/defaultfallback")
+    public Map<String,String> defaultfallback(){
+        System.out.println("降级操作...");
+        Map<String,String> map = new HashMap<>();
+        map.put("resultCode","fail");
+        map.put("resultMessage","服务异常");
+        map.put("resultObj","null");
+        return map;
+    }
+}
